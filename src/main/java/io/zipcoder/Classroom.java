5269acd45a6ba6439a82c02b104303b4bf81aae0
moffd234 <moffd234@gmail.com>
2024-03-15 12:47:42 -0400

    Created Classroom instance variables and getClassroomAverage

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,29 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Classroom {
 
+    ArrayList<Student> students;
+    int maxNumStudents;
+    public Classroom(ArrayList<Student> students) {
+        this.students = students;
+    }
+    public Classroom(int maxNumStudents){
+        this.maxNumStudents = maxNumStudents;
+    }
+    public Classroom(){}
+
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore() {
+        double sum = 0;
+        for(Student i : students){
+            sum += i.getAverageExamScore();
+        }
+        return sum / students.size();
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,31 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ClassroomTest {
+    @Test
+    public void testGetAverageExamScore(){
+        // : Given
+        ArrayList<Double> s1Scores = new ArrayList<>(Arrays.asList(100.0, 150.0));
+        ArrayList<Double> s2Scores = new ArrayList<>(Arrays.asList(225.0, 25.0));
+
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+
+        ArrayList<Student> students = new ArrayList<>(Arrays.asList(s1,s2));
+        Classroom classroom = new Classroom(students);
+
+        Double expectedAvg = (s1.getAverageExamScore() + s2.getAverageExamScore()) / 2;
+
+
+        // When
+        Double output = classroom.getAverageExamScore();
+
+        // Then
+        Assert.assertEquals(expectedAvg, output);
+    }
 }
