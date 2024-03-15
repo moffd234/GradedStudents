66dd5ec1d7346139d030fa25c5934aea4745f6fb
moffd234 <moffd234@gmail.com>
2024-03-14 22:38:07 -0400

    Created first two studentTests and their corresponding methods

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,44 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, ArrayList<Double> examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getExamScores(){
+        return examScores.toString();
+    }
+
+    public int getNumberOfExamsTaken(){
+        return examScores.size();
+    }
+
+    public void addExamScore(double v) {
+        examScores.add(v);
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,41 @@
 package io.zipcoder;
 
-public class StudentTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StudentTest {
+    @Test
+    public void getExamScores(){
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        ArrayList<Double> examScores = new ArrayList<>(Arrays.asList(100.0, 95.0, 123.0, 96.0));
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.getExamScores();
+
+        // Then
+        Assert.assertEquals(output, examScores.toString());
+    }
+
+    @Test
+    public void testAddExamScore(){
+    // : Given
+    String firstName = "Leon";
+    String lastName = "Hunter";
+    ArrayList<Double> examScores = new ArrayList<>();
+    Student student = new Student(firstName, lastName, examScores);
+
+    // When
+    student.addExamScore(100.0);
+    String output = student.getExamScores();
+
+    // Then
+    Assert.assertEquals(output, examScores.toString());
+    }
 }
